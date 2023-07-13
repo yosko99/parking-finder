1221b9d83795a008edc824060cab247e5f01e895
yosko99 <thedriftmaniaka1997@gmail.com>
2023-07-13 18:31:49 +0300

    Add border to parking marker

--- frontend/src/styles/global.css.ts
+++ frontend/src/styles/global.css.ts
@@ -19,6 +19,11 @@ const GlobalCSS = createGlobalStyle`
 	button.gm-ui-hover-effect {
     visibility: hidden;
 	}
+	.gm-style-iw {
+		box-shadow: 0 1px 6px rgba(178, 178, 178, 0.6);
+		border: 1px solid rgba(72, 181, 233, 0.6);
+		border-radius: 2px 2px 10px 10px;
+	}
     `;
 
 export default GlobalCSS;
